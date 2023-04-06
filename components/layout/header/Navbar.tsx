fa08acfe2c7ecba990c699f17444a92e9d4adeee
Mohmaed-Echbiy <echbiymed@gmail.com>
2023-04-06 22:24:59 +0000

    fix Navbar

--- components/layout/header/Navbar.tsx
+++ components/layout/header/Navbar.tsx
@@ -9,7 +9,7 @@ function Navbar() {
   return (
     <nav className='flex-grow '>
       <ul className='flex items-center justify-end gap-4 w-full'>
-        <li className='project-1 flex items-center gap-2 group'>
+        {/* <li className='project-1 flex items-center gap-2 group'>
           <FaSlackHash className='group-hover:text-accent ease-in-out duration-300 transition-colors w-5 h-5' />
           <Link
             className='group-hover:text-accent ease-in-out duration-300 transition-colors '
@@ -35,7 +35,7 @@ function Navbar() {
           >
             Project 3
           </Link>
-        </li>
+        </li> */}
         <li className='contact-me flex items-center gap-2 w-fit group'>
           <AiFillPhone className=' rotate-90 w-5 h-5 group-hover:text-accent ease-in-out duration-300 transition-colors' />
           <Link
